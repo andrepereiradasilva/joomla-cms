345ec8233aeee56725a99f50d033303406d1bcbd
Wojciech Smoliński <wojsmol@wp.pl>
2016-04-01 18:20:35 +0200

    rebase 3.5.1-2016-03-25.sql

--- administrator/components/com_admin/sql/updates/mysql/3.5.1-2016-03-25.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.1-2016-03-25.sql
@@ -1,3 +1,13 @@
+--
+-- Make #__user_keys.user_id fit to #__users.username
+--
+
+ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;
+
+--
+-- Update core #__update_sites entries to https
+--
+
 UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/list.xml' WHERE `name` = 'Joomla! Core' AND `type` = 'collection';
 UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/jed/list.xml' WHERE `name` = 'Joomla! Extension Directory' AND `type` = 'collection';
 UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/language/translationlist_3.xml' WHERE `name` = 'Accredited Joomla! Translations' AND `type` = 'collection';
