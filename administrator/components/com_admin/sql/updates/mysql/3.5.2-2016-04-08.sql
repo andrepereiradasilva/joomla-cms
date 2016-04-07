19d5e95d98fe90a771bc852735f4fb5deb5fc6b5
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-04-07 18:35:01 +0100

    Update 3.5.2-2016-04-08.sql

--- administrator/components/com_admin/sql/updates/mysql/3.5.2-2016-04-08.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.2-2016-04-08.sql
@@ -2,5 +2,5 @@
 INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`)
  VALUES (602, 'English (United Kingdom)', 'package', 'en-GB', '', 0, 1, 1, 1, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
--- change update site extension id.
+-- Change update site extension id to the new extension.
 UPDATE `#__update_sites_extensions` SET `extension_id` = 602 WHERE `update_site_id` = 3 AND `extension_id` = 600;
