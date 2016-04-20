438edbf8bdbae2d6d87c79388e9c277d02024fb6
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-04-21 00:12:16 +0100

    replace in assets table too

--- administrator/components/com_admin/sql/updates/mysql/3.5.2-2016-04-20.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.2-2016-04-20.sql
@@ -1,2 +1,3 @@
 UPDATE `#__categories` SET `extension` = 'com_users.notes' WHERE `extension` = 'com_users';
 UPDATE `#__content_types` SET `type_alias` = 'com_users.notes.category' WHERE `type_alias` = 'com_users.category';
+UPDATE `#__assets` SET `name` = REPLACE(`name`, 'com_users.category', 'com_users.notes.category') WHERE `name` LIKE 'com_users.category%';
