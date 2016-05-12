0254f8c7e7cec29c56e37723c106105a3c29951d
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-12 16:13:26 +0100

    add placeholders to offline

--- templates/protostar/offline.php
+++ templates/protostar/offline.php
@@ -117,14 +117,14 @@ else
 				<form action="<?php echo JRoute::_('index.php', true); ?>" method="post" id="form-login">
 					<fieldset>
 						<label for="username"><?php echo JText::_('JGLOBAL_USERNAME'); ?></label>
-						<input name="username" id="username" type="text" title="<?php echo JText::_('JGLOBAL_USERNAME'); ?>" />
+						<input name="username" id="username" type="text" placeholder="<?php echo JText::_('JGLOBAL_USERNAME'); ?>" />
 
 						<label for="password"><?php echo JText::_('JGLOBAL_PASSWORD'); ?></label>
-						<input type="password" name="password" id="password" title="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>" />
+						<input type="password" name="password" id="password" placeholder="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>" />
 
 						<?php if (count($twofactormethods) > 1) : ?>
 						<label for="secretkey"><?php echo JText::_('JGLOBAL_SECRETKEY'); ?></label>
-						<input type="text" name="secretkey" id="secretkey" title="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>" />
+						<input type="text" name="secretkey" id="secretkey" placeholder="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>" />
 						<?php endif; ?>
 
 						<input type="submit" name="Submit" class="btn btn-primary" value="<?php echo JText::_('JLOGIN'); ?>" />
