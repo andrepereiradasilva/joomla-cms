dfd12170acb6eac51b56d5e899b490791017af18
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2017-01-06 19:10:21 +0000

    Update renderfield.php

--- layouts/joomla/form/renderfield.php
+++ layouts/joomla/form/renderfield.php
@@ -19,7 +19,7 @@ extract($displayData);
  * 	$input           : (string) The input field html code
  */
 
-if (!empty($options['dataAttributes']['showon']))
+if (!empty($options['dataAttributes']['container']['showon']))
 {
 	JHtml::_('behavior.showon');
 }
@@ -27,9 +27,9 @@ if (!empty($options['dataAttributes']['showon']))
 $class = empty($options['class']) ? '' : ' ' . $options['class'];
 $rel   = empty($options['rel']) ? '' : ' ' . $options['rel'];
 ?>
-<div class="control-group<?php echo $class; ?>"<?php echo $rel; ?><?php echo JFormHelper::renderDataAttributes($options['dataAttributes']); ?>>
+<div class="control-group<?php echo $class; ?>"<?php echo $rel; ?><?php echo JFormHelper::renderDataAttributes($options['dataAttributes']['container']); ?>>
 	<?php if (empty($options['hiddenLabel'])) : ?>
-		<div class="control-label"><?php echo $label; ?></div>
+		<div class="control-label"<?php echo JFormHelper::renderDataAttributes($options['dataAttributes']['label']); ?>><?php echo $label; ?></div>
 	<?php endif; ?>
-	<div class="controls"><?php echo $input; ?></div>
+	<div class="controls"<?php echo JFormHelper::renderDataAttributes($options['dataAttributes']['field']); ?>><?php echo $input; ?></div>
 </div>
