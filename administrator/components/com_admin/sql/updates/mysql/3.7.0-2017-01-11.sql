3766607468e55a8536bce360d75f80fa425f4183
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2017-01-08 20:30:52 +0000

    some mroe changes

---
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2017-01-11.sql
@@ -0,0 +1,23 @@
+-- Normalize assets table default values.
+
+-- Normalize associations table default values.
+
+-- Normalize banners table default values.
+
+-- Normalize banner_clients table default values.
+
+-- Normalize banner_tracks table default values.
+
+-- Normalize contact_details table default values.
+
+-- Normalize content table default values.
+
+-- Normalize content_frontpage table default values.
+
+-- Normalize content_rating table default values.
+
+-- Normalize content_types table default values.
+
+-- Normalize contentitem_tag_map table default values.
+
+-- Normalize extensions table default values.
